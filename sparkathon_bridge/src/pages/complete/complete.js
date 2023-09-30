be83bc1e60f24e754edb205566edcc1d2b3cbbf5
Liew Chern Jin <69305479+codyy6@users.noreply.github.com>
2023-09-30 23:49:17 +0800

    final touch up

--- sparkathon_bridge/src/pages/complete/complete.js
+++ sparkathon_bridge/src/pages/complete/complete.js
@@ -3,6 +3,7 @@ import NavigationBar from '../../components/topNav'
 import { BrowserProvider } from 'ethers';
 import "./complete.css"
 import SurveyPOAP from "../../images/SurveyPOAP.png"
+import axios from 'axios';
 
 function Complete({ userAddress: propsUserAddress }) {
   const [userAddress, setAddress] = useState("");
@@ -12,6 +13,31 @@ function Complete({ userAddress: propsUserAddress }) {
     // Call getInformation when the component mounts
     getInformation();
   }, []);
+  const options = {
+    method: 'POST',
+    url: 'https://api.poap.tech/actions/claim-qr',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5qQTNOalpGUWpkRE9ESTNRa0V3UlVSRE9VVkVNRVUxT1VVd1JrSTNNRGs1TlRORVFqUTNSUSJ9.eyJpc3MiOiJodHRwczovL2F1dGguYWNjb3VudHMucG9hcC54eXovIiwic3ViIjoiVGRncGVkYXAxY0FodVNwbzJXeWQ0QnFTRlQ4RGV0WEdAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLnBvYXAudGVjaCIsImlhdCI6MTY5NjA0NjI5NSwiZXhwIjoxNjk2MTMyNjk1LCJhenAiOiJUZGdwZWRhcDFjQWh1U3BvMld5ZDRCcVNGVDhEZXRYRyIsInNjb3BlIjoibWludCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbIm1pbnQiXX0.jRjrQIw1PXjAKLztFvg8pHszwwNHXFuOSPHzvtfwPxpmmOxilSITvRPl_eHViSfrZpybBLO79uwXyL929iaeeU1d9jZN3Cg_tQH33-EPpKGf-Fy5obF_QTGCkmv7uENoW901EhILIyy4yt1bHxFfEm6ijhY_Xonqic8gBACGfOAWOOcqIYJG3T4d8_YL4KG2103AF5T6Y7Tr5beXIhkNXyZl6TOp7C2rjbXmf60TBAHHcX2pPBIeoRpuJ7quG4neKDk0S0haJY4RwipJY_KEyQRKrk_Tnmom0GB1wSzHCp_4UcB7AwwhVpOHZ3RWqrzMqhQTsc-zC0SY6AiOyyLRpw',
+      'x-api-key': 'h68Y0m9sK4HfODoRrqDPprKrHHTMgmdXxbWTEt8X70TgPM8MugH7zrsbQgS2smKTMJpgUKSQF888UM9UuUX4vlG0kaGD6kiITZNw5DyS0yx8MuMo0qfSPdOFOb1K2URu'
+    },
+    data: {
+      sendEmail: false,
+      address: userAddress,
+      qr_hash: 'wrq8qv',
+      secret: 'ab9fc58bbb84bfcfc903e90138f91ca3657e96667f88542d5aa96e3b46fbfa8a'
+    }
+  };
+  
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 
   async function getInformation() {
     try {
@@ -49,7 +75,7 @@ function Complete({ userAddress: propsUserAddress }) {
       <p className="header">{ensName || userAddress}</p>
       <div className="container">
         <div className="circleimage-container">
-          <a href='https://app.poap.xyz/token/6800192' target="_blank">
+        <a href={`https://app.poap.xyz/scan/${userAddress}`} target="_blank">
           <img className="circleimage" src={SurveyPOAP} alt="Survey POAP" /></a>
         </div>
         <p className="paragraph">
--- sparkathon_bridge/src/pages/home/home.js
+++ sparkathon_bridge/src/pages/home/home.js
@@ -1,29 +1,86 @@
-import NavigationBar from '../../components/topNav'
-import React from 'react'; // Import useState and useEffect
-import { Link } from 'react-router-dom'; // Import the Link component
+import React, { useState, useEffect } from 'react';
+import NavigationBar from '../../components/topNav';
+import { Link, useNavigate } from 'react-router-dom';
 import "./home.css";
+import axios from 'axios';
 
 function Main() {
+  const [userAddress, setAddress] = useState("");
+  const [poapExist, setPoapExist] = useState(false);
+  const navigate = useNavigate();
 
-return ( 
-  <div>
-    <NavigationBar />
-    <div className="container">
-      <div className="card">
-        <Link to="/survey" className='cardLink'>
-          <h3><i><u>Gnosis Survey by Gnosis Team</u></i></h3>
-          <p>A Survey to understand more about users' opinion on Gnosis Chain and potential future enhancement.</p>
-        </Link>
+  useEffect(() => {
+    getInformation();
+  }, []);
+
+  async function getInformation() {
+    try {
+      const res = await fetch(`http://localhost:3000/personal_information`, {
+        credentials: 'include',
+      });
+
+      if (!res.ok) {
+        console.error(`Failed in getInformation: ${res.statusText}`);
+        return;
+      }
+
+      const result = await res.text();
+      const address = result.split(" ")[result.split(" ").length - 1];
+      setAddress(address);
+    } catch (error) {
+      console.error(`Error in getInformation: ${error}`);
+    }
+  }
+
+  const options = {
+    method: 'GET',
+    url: `https://api.poap.tech/actions/scan/${userAddress}/150793`,
+    headers: {
+      accept: 'application/json',
+      'x-api-key': 'h68Y0m9sK4HfODoRrqDPprKrHHTMgmdXxbWTEt8X70TgPM8MugH7zrsbQgS2smKTMJpgUKSQF888UM9UuUX4vlG0kaGD6kiITZNw5DyS0yx8MuMo0qfSPdOFOb1K2URu'
+    }
+  };
+
+  useEffect(() => {
+    // Make the axios request here when the component mounts
+    axios
+      .request(options)
+      .then(function (response) {
+        setPoapExist(true);
+      })
+      .catch(function (error) {
+        setPoapExist(false);
+      });
+  }, [options]);
+
+  function isPoapExist() {
+    if (poapExist === false) {
+      navigate('/survey');
+      console.log("Testing navigation to /survey");
+    } else {
+      alert("Survey already done");
+    }
+  }
+
+  return (
+    <div>
+      <NavigationBar />
+      <div className="container">
+        <div className="card">
+          <Link className='cardLink' onClick={isPoapExist}>
+            <h3><i><u>Gnosis Survey by Gnosis Team</u></i></h3>
+            <p>A Survey to understand more about users' opinion on Gnosis Chain and potential future enhancement.</p>
+          </Link>
         </div>
-      <div className="card">
-      <Link to="/survey" className='cardLink'>
-        <h3><i><u>POAP Survey by POAP Team</u></i></h3>
-        <p>A Survey to understand more about users' opinion on Gnosis Chain and potential future enhancement.</p>
-      </Link>
+        <div className="card">
+          <Link to="/survey" className='cardLink'>
+            <h3><i><u>POAP Survey by POAP Team</u></i></h3>
+            <p>A Survey to understand more about users' opinion on Gnosis Chain and potential future enhancement.</p>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default Main;
