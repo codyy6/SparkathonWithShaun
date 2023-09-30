f8502bfcb531ba2105858df4e76780ac823fd759
Liew Chern Jin <69305479+codyy6@users.noreply.github.com>
2023-09-30 14:00:36 +0800

    remove unnecessary files and testing claim POAP

--- sparkathon_bridge/src/pages/survey/survey.js
+++ sparkathon_bridge/src/pages/survey/survey.js
@@ -4,6 +4,21 @@ import "./survey.css"
 import { BrowserProvider } from 'ethers';
 import { Link } from 'react-router-dom'; // Import the Link component
 
+function claimPoap(){
+  const sdk = require('api')('@poap/v1.0#lcw4v2g2llldijtcg');
+
+  sdk.auth('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5qQTNOalpGUWpkRE9ESTNRa0V3UlVSRE9VVkVNRVUxT1VVd1JrSTNNRGs1TlRORVFqUTNSUSJ9.eyJpc3MiOiJodHRwczovL2F1dGguYWNjb3VudHMucG9hcC54eXovIiwic3ViIjoiVGRncGVkYXAxY0FodVNwbzJXeWQ0QnFTRlQ4RGV0WEdAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLnBvYXAudGVjaCIsImlhdCI6MTY5NjA0NjI5NSwiZXhwIjoxNjk2MTMyNjk1LCJhenAiOiJUZGdwZWRhcDFjQWh1U3BvMld5ZDRCcVNGVDhEZXRYRyIsInNjb3BlIjoibWludCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbIm1pbnQiXX0.jRjrQIw1PXjAKLztFvg8pHszwwNHXFuOSPHzvtfwPxpmmOxilSITvRPl_eHViSfrZpybBLO79uwXyL929iaeeU1d9jZN3Cg_tQH33-EPpKGf-Fy5obF_QTGCkmv7uENoW901EhILIyy4yt1bHxFfEm6ijhY_Xonqic8gBACGfOAWOOcqIYJG3T4d8_YL4KG2103AF5T6Y7Tr5beXIhkNXyZl6TOp7C2rjbXmf60TBAHHcX2pPBIeoRpuJ7quG4neKDk0S0haJY4RwipJY_KEyQRKrk_Tnmom0GB1wSzHCp_4UcB7AwwhVpOHZ3RWqrzMqhQTsc-zC0SY6AiOyyLRpw');
+  sdk.auth('h68Y0m9sK4HfODoRrqDPprKrHHTMgmdXxbWTEt8X70TgPM8MugH7zrsbQgS2smKTMJpgUKSQF888UM9UuUX4vlG0kaGD6kiITZNw5DyS0yx8MuMo0qfSPdOFOb1K2URu');
+  sdk.pOSTActionsClaimQr({
+    sendEmail: false,
+    secret: 'ab9fc58bbb84bfcfc903e90138f91ca3657e96667f88542d5aa96e3b46fbfa8a',
+    address: '',
+    qr_hash: 'wrq8qv'
+})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+}
+
 function Survey({ userAddress: propsUserAddress }){
   const [userAddress, setAddress] = useState("");
   const [ensName, setEnsName] = useState("");
@@ -116,7 +131,7 @@ function Survey({ userAddress: propsUserAddress }){
           </div>
         </div>
         <Link to="/complete">
-        <button type="submit" className="button">Submit</button>
+          <button type="submit" className="button" onClick={claimPoap}>Submit</button>
         </Link>
         </form>
       </div>
